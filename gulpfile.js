17d60da1e9df146fb1dff94375355e7b6da3c5c2
iyadk <iyadk@skitsc.com>
2021-06-02 13:34:01 -0400

    added img minifier

--- gulpfile.js
+++ gulpfile.js
@@ -18,13 +18,15 @@ var gulpif = require('gulp-if');
 var uglify = require('gulp-uglify');
 var obfuscator = require('gulp-javascript-obfuscator');
 
+var imagemin = require('gulp-imagemin');
+
 var browserSync = require('browser-sync').create();
 
 var del = require('del');
 
 gulp.task('clean', done => {
 
-    del.sync(['/static/*']);
+    del.sync(['/src/static/*']);
     done();
 });
 
@@ -32,12 +34,14 @@ gulp.task('watch', () => {
 
     browserSync.init({
         server: {
-            baseDir: "./static"
+            baseDir: "./src/static"
         }
     });
 
-    gulp.watch('./public/scss/*.scss').on('change', gulp.series('sass', 'css')); //compile sass and minify css
-    gulp.watch('./public/js/*.js').on('change', gulp.series('script')); //uglify js
+    gulp.watch('./public/scss/*.scss').on('change', gulp.series('sass')); //compile sass and minify css
+    gulp.watch('./public/js/*.js').on('change', gulp.series('scripts')); //uglify and obfuscate js
+    gulp.watch('./public/fonts/*').on('change', gulp.series('fonts')); //move fonts
+    gulp.watch('./public/img/*').on('change', gulp.series('images')); //minify images
 });
 
 gulp.task('sass', () => {
@@ -48,25 +52,32 @@ gulp.task('sass', () => {
         .pipe(sourcemaps.write())
         .pipe(cleancss())
         .pipe(rename({suffix:'.min'}))
-        .pipe(gulp.dest('./static/css/'));
+        .pipe(gulp.dest('./src/static/css/'));
 });
 
-gulp.task('script', () => {
+gulp.task('scripts', () => {
 
     return gulp.src('./public/js/*.js')
         .pipe(gulpif('*.js', uglify()))
         .pipe(obfuscator({compact:true}))
         .pipe(rename({suffix:'.min'}))
-        .pipe(gulp.dest('./static/js/'));
+        .pipe(gulp.dest('./src/static/js/'));
 });
 
 gulp.task('fonts', () => {
   
     return gulp.src('./public/fonts/*.{eot, svg, ttf, woff, woff2}')
-        .pipe(gulp.dest('./static/fonts/'));
+        .pipe(gulp.dest('./src/static/fonts/'));
 });
 
-gulp.task('build', gulp.series('clean', 'sass', 'script', 'fonts'), () => {
+gulp.task('images', () => {
+
+    return gulp.src('./public/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./src/static/img/'));
+});
+
+gulp.task('build', gulp.series('clean', 'sass', 'scripts', 'fonts', 'images'), () => {
     
-    return gulp.dest('./static');
+    return gulp.dest('./src/static');
 });
